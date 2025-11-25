ce3e8381ff7f52ab3117e877ebea6d219db91daa
Nasty289 <Nasty.289@web.de>
2025-11-25 11:08:43 +0100

    Spruce Stick

--- src/main/java/net/nasty289/woodvariants/WoodVariants.java
+++ src/main/java/net/nasty289/woodvariants/WoodVariants.java
@@ -2,6 +2,7 @@ package net.nasty289.woodvariants;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.nasty289.woodvariants.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,5 +13,6 @@ public class WoodVariants implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/nasty289/woodvariants/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.nasty289.woodvariants.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.nasty289.woodvariants.WoodVariants;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static final Item SPRUCE_STICK = registerItem("spruce_stick", setting -> new Item(setting));
+
+    private static Item registerItem(String name, Function<Item.Settings, Item> function) {
+        return Registry.register(Registries.ITEM, Identifier.of(WoodVariants.MOD_ID, name),
+                function.apply(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(WoodVariants.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+        WoodVariants.LOGGER.info("Registering Mod Items for " + WoodVariants.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(SPRUCE_STICK);
+        });
+    }
+}
