fdf65ff53794883db284965de4ac135b26efc679
Nasty289 <Nasty.289@web.de>
2025-11-25 19:13:28 +0100

    All Sticks added

--- src/main/java/net/nasty289/woodvariants/item/ModItems.java
+++ src/main/java/net/nasty289/woodvariants/item/ModItems.java
@@ -15,8 +15,15 @@ import java.util.function.Function;
 public class ModItems {
 
     public static final Item SPRUCE_STICK = registerItem("spruce_stick", setting -> new Item(setting));
-
+    public static final Item BIRCH_STICK = registerItem("birch_stick", setting -> new Item(setting));
     public static final Item ACACIA_STICK = registerItem("acacia_stick", setting -> new Item(setting));
+    public static final Item CHERRY_STICK = registerItem("cherry_stick", setting -> new Item(setting));
+    public static final Item CRIMSON_STICK = registerItem("crimson_stick", setting -> new Item(setting));
+    public static final Item DARK_OAK_STICK = registerItem("dark_oak_stick", setting -> new Item(setting));
+    public static final Item JUNGLE_STICK = registerItem("jungle_stick", setting -> new Item(setting));
+    public static final Item MANGROVE_STICK = registerItem("mangrove_stick", setting -> new Item(setting));
+    public static final Item PALE_OAK_STICK = registerItem("pale_oak_stick", setting -> new Item(setting));
+    public static final Item WARPED_STICK = registerItem("warped_stick", setting -> new Item(setting));
 
     private static Item registerItem(String name, Function<Item.Settings, Item> function) {
         return Registry.register(Registries.ITEM, Identifier.of(WoodVariants.MOD_ID, name),
@@ -28,7 +35,15 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(SPRUCE_STICK);
+            entries.add(BIRCH_STICK);
             entries.add(ACACIA_STICK);
+            entries.add(CHERRY_STICK);
+            entries.add(CRIMSON_STICK);
+            entries.add(DARK_OAK_STICK);
+            entries.add(JUNGLE_STICK);
+            entries.add(MANGROVE_STICK);
+            entries.add(PALE_OAK_STICK);
+            entries.add(WARPED_STICK);
         });
     }
 }
